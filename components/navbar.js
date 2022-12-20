bf15aa71068d7b7c4dfba2c0e3c12589fbecf440
calgui1 <77653871+calgui1@users.noreply.github.com>
2022-12-20 01:53:08 -0600

    Added navigation bar

---
+++ components/navbar.js
@@ -0,0 +1,123 @@
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue,
+  Text
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { IoLogoGithub } from 'react-icons/io5'
+
+const LinkItem = ({ href, path, target, children, ...props }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray200','whiteAlpha.900')
+  return (
+    <NextLink href={href} passHref scroll={false}>
+      <Link
+        p={2}
+        bg={active ? 'grassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+        target={target}
+        {...props}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
+}
+
+const NavBar = props => {
+  const { path } = props
+
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      css={{ backdropFilter: 'blur(10px)' }}
+      zIndex={2}
+      {...props}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            Game of Life
+          </Heading>
+        </Flex>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, md: 0 }}
+        >
+          <LinkItem href="/configs" path={path}>
+            Configurations
+          </LinkItem>
+          <LinkItem href="/about" path={path}>
+            About
+          </LinkItem>
+          <LinkItem 
+            target="_blank"
+            href="https://github.com/calgui1/game-of-life"
+            path={path}
+            display="inline-flex"
+            alignItems="center"
+            style={{ gap: 4 }}
+            pl={2}
+          >
+            <IoLogoGithub />Source Code
+          </LinkItem>
+        </Stack>
+
+        <Box flex={1} align="right">
+					<Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+						<Menu isLazy id="navbar-menu">
+							<MenuButton	
+								as={IconButton}
+								icon={<HamburgerIcon />}
+								variant="outline"
+								aria-label="Options"
+							/>
+							<MenuList>
+								<NextLink href="/" passHref>
+									<MenuItem as={Link}>Play</MenuItem>
+								</NextLink>
+								<NextLink href="/configs" passHref>
+									<MenuItem as={Link}>Configurations</MenuItem>
+								</NextLink>
+								<MenuItem	
+									as={Link}
+									href="https://github.com/calgui1/game-of-life"
+								>
+									Source Code
+								</MenuItem>
+							</MenuList>
+						</Menu>
+					</Box>
+				</Box>
+
+      </Container>
+    </Box>
+  )
+}
+
+export default NavBar
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import {
+  Box,
+  Button,
+  chakra,
+  Container,
+  Heading,
+  Link,
+  List,
+  ListItem,
+  SimpleGrid,
+  useColorModeValue
+} from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Container>
+      <Box>Hello</Box>
+    </Container>
+  )
+}
+
+export default Home
