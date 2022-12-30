b4d696478924b69c61917d18dc75b1d33c5a8503
calgui1 <77653871+calgui1@users.noreply.github.com>
2022-12-30 01:34:13 -0600

    Added header and sections to About page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,22 @@
+import { Container, Heading } from "@chakra-ui/react"
+import Section from "../components/section"
 
 const About = () => {
+  return (
+    <Container p={4} maxW='container.xl'>
+      <Section delay={0.1}>
+        <Heading as='h2' align='center' variant='section-title'>
+          About
+        </Heading>
+      </Section>
+      <Section delay={0.2}>
 
+      </Section>
+      <Section delay={0.3}>
+        
+      </Section>
+    </Container>
+  )
 }
 
 export default About
